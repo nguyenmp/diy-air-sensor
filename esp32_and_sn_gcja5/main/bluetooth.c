21b576c0b3e08db3582e6578a545f4f75e2bba22
Mark Nguyen <mnguyen4@apple.com>
2020-10-06 22:43:15 -0700

    Added a visible characteristic to the service

--- esp32_and_sn_gcja5/main/bluetooth.c
+++ esp32_and_sn_gcja5/main/bluetooth.c
@@ -110,6 +110,17 @@ void start_bluetooth_advertising() {
 
 #define PRIMARY_SERVICE_UUID BLE_UUID128_DECLARE( \
     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)
+#define PRIMARY_CHARACTERISTIC_UUID BLE_UUID128_DECLARE( \
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1)
+#define SECONDARY_CHARACTERISTIC_UUID BLE_UUID128_DECLARE( \
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2)
+
+int characteristic_access_callback(uint16_t conn_handle, uint16_t attr_handle, struct ble_gatt_access_ctxt *ctxt, void *arg) {
+    ESP_LOGI(TAG, "Accessed a characteristic...");
+
+    // This means a successful read
+    return 0;
+}
 
 // Struct as documented in:
 // * https://mynewt.apache.org/latest/network/ble_hs/ble_gatts.html#c.ble_gatt_svc_def
@@ -119,6 +130,11 @@ const struct ble_gatt_svc_def gatt_services[] = {
         .type = BLE_GATT_SVC_TYPE_PRIMARY,
         .uuid = PRIMARY_SERVICE_UUID,
         .characteristics = (struct ble_gatt_chr_def[]) {
+            {
+                .uuid = PRIMARY_CHARACTERISTIC_UUID,
+                .access_cb = characteristic_access_callback,
+                .flags = BLE_GATT_CHR_F_READ | BLE_GATT_CHR_F_READ_ENC,
+            },
             {
                 0, /* No more characteristics in this service. */
             },
