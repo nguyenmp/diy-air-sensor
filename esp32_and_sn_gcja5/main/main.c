360be42509b29669badcbd7697817f23747b80ea
Mark Nguyen <mnguyen4@apple.com>
2020-10-07 23:23:44 -0700

    Renamed hello_world_main.c to main.c because it's not hello world anymore

--- esp32_and_sn_gcja5/main/main.c
+++ esp32_and_sn_gcja5/main/main.c
@@ -1,11 +1,4 @@
-/* Hello World Example
-
-   This example code is in the Public Domain (or CC0 licensed, at your option.)
-
-   Unless required by applicable law or agreed to in writing, this
-   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-   CONDITIONS OF ANY KIND, either express or implied.
-*/
+/* Based on Hello World Example from esp-idf */
 #include "uart.h"
 #include "aqi.h"
 #include "bluetooth.h"
